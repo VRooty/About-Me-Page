26d94f2ac725542f347fa2d6a0e381de9a5653a9
VRooty <144456722+VRooty@users.noreply.github.com>
2023-09-19 16:52:32 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+let Username = prompt('What's your name?')
+    alert("Hi "+ Username +". Nice to meet you.")
+    console.log ("User's name is " + Username)
+
+
+
+
+
+let answer1 = True
+let answer2 = False
+let answer3 = False
+let answer4 = True 
+let answer5 = False
+
+function myFunction() {
+  let answer1 = prompt("Is Vicky's surname Rooney?");
+  console.log("Vicky has 3 surnames")
+
+
+if (answer=="No" || answer1=="Yes") {
+    alert("Either is correct.")}
+        
+
+let answer2 = prompt("Does Vicky know Arabic?")
+
+if ("no")
+    alert("Right, she never got passed the alphabet")
+else:
+    alert("no, that's not correct")
+    console.log("User guessed" + answer2)
+
+let answer3 = prompt("Vicky has a cat named Kira")
+
+
+
+let answer4 = prompt("Vicky's favourite game on the PS2 is Silent Hill")
+}
+
+let answer5 = prompt("Vicky would like to work for Google")
