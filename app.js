167f95c17082af0b064297418f226ed97e27a1e0
Samuel Draper <samueledraper@outlook.com>
2023-09-21 14:50:16 +0100

    add 5 question quiz

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
-let Username = prompt('What's your name?')
-    alert("Hi "+ Username +". Nice to meet you.")
-    console.log ("User's name is " + Username)
-
-
-
-
-
-let answer1 = True
-let answer2 = False
-let answer3 = False
-let answer4 = True 
-let answer5 = False
+let Username = prompt("What's your name?");
+alert("Hi " + Username + ". Nice to meet you.");
+console.log("User's name is " + Username);
 
 function myFunction() {
-  let answer1 = prompt("Is Vicky's surname Rooney?");
-  console.log("Vicky has 3 surnames")
+  let question1 = prompt("Is Vicky's surname Rooney?");
+  if (question1 === "yes" || question1 === "no") {
+    alert("Either is correct. Vicky has 3 surnames");
+  }
 
+  let question2 = prompt("Does Vicky know Arabic?");
+  if (question2 === "no") {
+    alert("Right, she never got passed the alphabet");
+  } else {
+    alert("no, that's not correct");
+  }
 
-if (answer=="No" || answer1=="Yes") {
-    alert("Either is correct.")}
-        
+  let question3 = prompt("Does Vicky have a cat named Kira?");
+  if (question3 === "no") {
+    alert("Thats the correct answer, Kira is her daughter.");
+  } else {
+    alert("Thats incorrect, Kira is her daughter.");
+  }
 
-let answer2 = prompt("Does Vicky know Arabic?")
+  let question4 = prompt("Is Vicky's favourite game on the PS2 Silent Hill?");
+  if (question4 === "yes") {
+    alert(
+      "Thats the correct answer, Vicky's favourite game on the PS2 Silent Hill."
+    );
+  } else {
+    alert("Thats incorrect.");
+  }
 
-if ("no")
-    alert("Right, she never got passed the alphabet")
-else:
-    alert("no, that's not correct")
-    console.log("User guessed" + answer2)
-
-let answer3 = prompt("Vicky has a cat named Kira")
-
-
-
-let answer4 = prompt("Vicky's favourite game on the PS2 is Silent Hill")
+  let question5 = prompt("Would Vicky would like to work for Google?");
+  if (question5 === "no") {
+    alert("Thats the correct answer, Vicky would not like to work for Google.");
+  } else {
+    alert("Thats incorrect.");
+  }
 }
-
-let answer5 = prompt("Vicky would like to work for Google")
